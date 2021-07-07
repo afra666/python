234462249baece6b0b0f66f1cbf417efdbbeed62
afra666 <48674984+afra666@users.noreply.github.com>
2021-07-07 21:33:48 +0800

    Create 025.生成随机六个字符.py

---
+++ 025.生成随机六个字符.py
@@ -0,0 +1,13 @@
+import random
+rand_0=random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+rand_1 = random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+rand_2 = random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+rand_3= random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+rand_4= random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+rand_5= random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+rand_6="-";
+print(f"{rand_0}{rand_1}{rand_2}{rand_3}{rand_4}{rand_5}{rand_6}{rand_0}{rand_1}{rand_2}{rand_3}{rand_4}{rand_5}");
+
+
+
+Y9B5pu-Y9B5pu
